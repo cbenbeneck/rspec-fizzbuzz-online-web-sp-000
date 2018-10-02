1cc84a26265d2239ec8f93eed806203e0d431818
Christina Benbeneck <cbenbeneck@mail.usf.edu>
2018-10-02 14:55:21 +0000

    automatically backed up by learn

--- fizzbuzz.rb
+++ fizzbuzz.rb
@@ -1,2 +1,16 @@
 # Don't forget! This file needs to be 'required' in its spec file
 # See README.md for instructions on how to do this
+
+def fizzbuzz(int)
+  if int % 3 == 0 #if the number int is divisble by 3
+  "Fizz" #Go fizz 
+   
+   elsif int % 5 == 0 #if the number int is divisble by 5
+   "Buzz" #Go buzz
+ 
+   elsif int % 15 == 0   #if the number in is divisble by 15
+   "FizzBuzz" #Go Fizzbuzz
+  end
+   
+   
+
